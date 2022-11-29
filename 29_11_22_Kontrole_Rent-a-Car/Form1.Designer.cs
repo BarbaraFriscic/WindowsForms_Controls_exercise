de857d61e3d09894d4b82408106f07f999f15284
Barbara Friščić <80264267+BarbaraFriscic@users.noreply.github.com>
2022-11-29 17:49:26 +0100

    Add project files.

---
+++ 29_11_22_Kontrole_Rent-a-Car/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace _29_11_22_Kontrole_Rent_a_Car
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCalcPrice = new System.Windows.Forms.Button();
+            this.lblShowReturn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(112, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rent a Car";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(56, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(201, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Broj dana:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Gradski",
+            "Limuzina",
+            "Karavan"});
+            this.listBox1.Location = new System.Drawing.Point(59, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(101, 52);
+            this.listBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox1.Location = new System.Drawing.Point(199, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // btnCalcPrice
+            // 
+            this.btnCalcPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnCalcPrice.Location = new System.Drawing.Point(117, 191);
+            this.btnCalcPrice.Name = "btnCalcPrice";
+            this.btnCalcPrice.Size = new System.Drawing.Size(128, 29);
+            this.btnCalcPrice.TabIndex = 5;
+            this.btnCalcPrice.Text = "Izračunaj cijenu";
+            this.btnCalcPrice.UseVisualStyleBackColor = true;
+            this.btnCalcPrice.Click += new System.EventHandler(this.btnCalcPrice_Click);
+            // 
+            // lblShowReturn
+            // 
+            this.lblShowReturn.AutoSize = true;
+            this.lblShowReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblShowReturn.Location = new System.Drawing.Point(59, 255);
+            this.lblShowReturn.Name = "lblShowReturn";
+            this.lblShowReturn.Size = new System.Drawing.Size(0, 20);
+            this.lblShowReturn.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 360);
+            this.Controls.Add(this.lblShowReturn);
+            this.Controls.Add(this.btnCalcPrice);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCalcPrice;
+        private System.Windows.Forms.Label lblShowReturn;
+    }
+}
+
---
+++ 29_11_22_Kontrole_Rent-a-Car/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _29_11_22_Kontrole_Rent_a_Car
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcPrice_Click(object sender, EventArgs e)
+        {
+
+            double cijena = 0;
+            try
+            {
+                
+                int brojDana = Convert.ToInt32(comboBox1.Text);
+                if (listBox1.SelectedItem.ToString() == "Gradski")
+                {
+                    cijena = 150 * brojDana;
+                    
+                }
+                else if (listBox1.SelectedItem.ToString() == "Limuzina")
+                {
+                    cijena = 250 * brojDana;
+                    
+                }
+                else if (listBox1.SelectedItem.ToString() == "Karavan")
+                {
+                    cijena = 500 * brojDana;
+                    
+                }
+                else
+                    cijena = 0;
+            }
+            catch (FormatException)
+            {
+
+                MessageBox.Show("Unesite ispravan broj!");
+            }
+            lblShowReturn.Text = $"Ukupna cijena: {cijena:C}";
+
+        }
+    }
+}
